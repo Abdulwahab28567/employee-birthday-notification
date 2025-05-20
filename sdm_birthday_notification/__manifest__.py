4f2a3d74547e0b073793b6cb7e45ba4824df2ccf
Abdelwahab Eldweik <148437891+Abdulwahab28567@users.noreply.github.com>
2025-05-20 11:54:50 +0300

    Add files via upload

---
+++ sdm_birthday_notification/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Employee Birthday Notifications',
+    'version': '17.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Notifications for upcoming employee birthdays',
+    'description': 'Displays notifications for upcoming employee birthdays in systray.',
+    'author': 'Abdelwahab Eldweik',
+    'website': 'https://www.linkedin.com/in/abdelwahab-nasser-4022862b9/',
+    'depends': ['base', 'hr'],
+    'data': [],
+    'assets': {
+        'web.assets_backend': [
+            'sdm_birthday_notification/static/src/xml/systray_dropdown.xml',
+            'sdm_birthday_notification/static/src/js/dropdown_systray.js',
+            'sdm_birthday_notification/static/src/css/style.css',
+        ],
+    },
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
